caf1ebfaa5dc94ddea97a364c54ee13278df7f81
IcecoolR <45355900+IcecoolR@users.noreply.github.com>
2025-09-03 21:28:20 +0100

    Added Leaflet
    Added leaflet library and added the pane to the Map View section. Added EU countries

--- assets/scripts.js
+++ assets/scripts.js
@@ -27,3 +27,40 @@ mapViewBtn.addEventListener("keydown", mapViewBtnHandler);
 
 aboutBtn.addEventListener("click", aboutBtnHandler);
 aboutBtn.addEventListener("keydown", aboutBtnHandler);
+
+
+
+/* Section for interactive map */
+
+const map = L.map('map');
+map.createPane('labels');
+map.setView({lat: 47.040182144806664, lng: 9.667968750000002}, 3);
+
+// This pane is above markers but below popups
+map.getPane('labels').style.zIndex = 650;
+
+// Layers in this pane are non-interactive and do not obscure mouse/touch events
+map.getPane('labels').style.pointerEvents = 'none';
+
+// Sets the cartodbAttribution variable
+const cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
+
+// Draws country boarders
+const positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+	attribution: cartodbAttribution
+}).addTo(map);
+
+// Adds country labels
+const positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+	attribution: cartodbAttribution,
+	pane: 'labels'
+}).addTo(map);
+
+// Countries
+const geojson = L.geoJson(countries).addTo(map);
+
+geojson.eachLayer((layer) => {
+  layer.bindPopup(layer.feature.properties.name);
+});
+
+
